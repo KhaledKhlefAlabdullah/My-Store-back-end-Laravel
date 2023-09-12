08f53e6d5c0bf94e06d166d606639f1bcb26e120
khaled alabdullah <khaledabdullah2001104@gmail.com>
2023-09-12 17:42:19 +0300

    upload laravel code to git hub

---
+++ app/Models/Product_images.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product_images extends Model
+{
+    use HasFactory;
+
+    // Set primary key data type : string
+    protected $keyType = 'string';
+
+    // Set incrementing : false
+    public $incrementing = false;
+
+    // Set id as primary key
+    protected $primaryKey ='id';
+
+    // The object attribute
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+}
